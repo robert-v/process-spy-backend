2c4073cd5dc97a43cc2821928e399c648227e605
Robert Varga <robert.varga@sap.com>
2024-02-26 16:04:04 +0100

    added real date for download

--- pages/api/downloads.tsx
+++ pages/api/downloads.tsx
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { sql } from '@vercel/postgres'
+import moment from "moment";
 
 type ResponseData = {
     message: string
@@ -10,7 +11,9 @@ export default async function handler(
     res: NextApiResponse<ResponseData>
 ) {
     if (req.method == "POST"){
-        let data = await sql`INSERT INTO downloads (DateTime) VALUES ('2024-02-02 10:05')`
+        let formatted = moment(new Date()).format('YYYY-MM-DD hh:mm')
+        let data = await sql`INSERT INTO downloads (DateTime) VALUES (${formatted})`
+        res.status(200).json({ message: data.rows.length.toString() })
     }
     else if (req.method == "GET") {
         var data = await sql`select * from downloads`
