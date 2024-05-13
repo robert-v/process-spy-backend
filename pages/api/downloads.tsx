54c0e62e5cd14bc36c508a203efeea6b5a04afbc
Robert Varga <varga.robert@gmail.com>
2024-05-13 13:33:02 +0200

    added real date for download

--- pages/api/downloads.tsx
+++ pages/api/downloads.tsx
@@ -10,6 +10,16 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
     if (req.method == "POST"){
         let formatted = moment(new Date()).format('YYYY-MM-DD HH:mm')
         let data = await sql`INSERT INTO downloads (DateTime) VALUES (${formatted})`
